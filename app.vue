5cfda406c1d9627ae1dd5757b8ccd9eb2a8060e5
Adam DeHaven <adam@dehaven.org>
2024-04-22 20:27:28 -0400

    chore: log all values

--- app.vue
+++ app.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
-    <h1>Origin</h1>
+    <h2>Server</h2>
     <p>The <b>origin</b> is: <code>{{ data.origin }}</code></p>
     <p>The <b>hostname</b> is: <code>{{ data.hostname }}</code></p>
+    <h2>Client</h2>
     <ClientOnly>
+      <p>The <b>clientOrigin</b> is: <code>{{ clientOrigin }}</code></p>
       <p>The <b>clientHostname</b> is: <code>{{ clientHostname }}</code></p>
+      <p>These are the values I would expect to be returned in the <b>Server</b> section above.</p>
     </ClientOnly>
   </div>
 </template>
@@ -13,7 +16,9 @@
 const { data } = useFetch('/api/origin')
 
 const clientHostname = useState('client-hostname', () => '')
+const clientOrigin = useState('client-origin', () => '')
 onMounted(() => {
+  clientOrigin.value = window.location.origin
   clientHostname.value = window.location.hostname
 })
 </script>
